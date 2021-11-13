1e7c7ae715d68e407252966a2c8bc8f144a8b5d3
Hongyuan <hongyuan0104@gmail.com>
2021-11-13 21:20:20 +0000

    update comment

--- topInterviewQuestionsEasy/linkedList/DeleteNodeInALinkedList.py
+++ topInterviewQuestionsEasy/linkedList/DeleteNodeInALinkedList.py
@@ -2,6 +2,9 @@ from imports import *
 
 class Solution:
     """
+    Runtime: 40 ms, faster than 67.97% of Python3 online submissions for Delete Node in a Linked List.
+    Memory Usage: 14.9 MB, less than 31.15% of Python3 online submissions for Delete Node in a Linked List.
+    
     Interesting problem but not interesting problem.
     """
     def deleteNode(self, node):
